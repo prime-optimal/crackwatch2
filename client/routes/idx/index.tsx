6266548522ed0bb6297b51d9dac9d5d1c703dd6b
Donatas Bucinskas <donatas@tronikel.com>
2022-02-05 12:07:51 +0200

    add schema

--- client/routes/idx/index.tsx
+++ client/routes/idx/index.tsx
@@ -1,13 +1,27 @@
 import WhatshotIcon from "@mui/icons-material/Whatshot";
-import { Container, Grid } from "@mui/material";
-import useSWR from "swr";
+import { CircularProgress, Container, Grid, Stack } from "@mui/material";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import useSWRInfinite, { SWRInfiniteKeyLoader } from "swr/infinite";
 
 import { AxiosGamesPopular } from "@types";
 
 import { GameCard, IconTypography } from "@components";
 
+const getKey: SWRInfiniteKeyLoader = (index, previous) => {
+    if (previous && !previous.next) return null;
+    return `/games/popular?page=${index + 1}`;
+};
+
 export default function Popular() {
-    const { data } = useSWR<AxiosGamesPopular>("/games/popular");
+    const { data, setSize } = useSWRInfinite<AxiosGamesPopular>(getKey);
+
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (!inView) return;
+        setSize(x => x + 1);
+    }, [inView, setSize]);
 
     return (
         <Container maxWidth="lg" sx={{ mt: 3 }}>
@@ -20,8 +34,8 @@ export default function Popular() {
             </IconTypography>
 
             <Grid container spacing={6}>
-                {data?.results.map(
-                    ({ id, name, background_image, clip, genres, metacritic }) => (
+                {data?.map(({ results }) =>
+                    results.map(({ id, name, background_image, clip, genres, metacritic }) => (
                         <Grid item xs={12} md={6} lg={4} key={id}>
                             <GameCard
                                 genres={genres.map(x => x.name)}
@@ -31,9 +45,19 @@ export default function Popular() {
                                 metacritic={metacritic ? metacritic : undefined}
                             />
                         </Grid>
-                    )
+                    ))
                 )}
             </Grid>
+
+            <Stack
+                p={3}
+                justifyContent="center"
+                alignItems="center"
+                mt={1}
+                ref={data ? ref : undefined}
+            >
+                <CircularProgress />
+            </Stack>
         </Container>
     );
 }
--- server/src/routes/games/popular.ts
+++ server/src/routes/games/popular.ts
@@ -1,10 +1,24 @@
 import axios from "axios";
-import { RouteOptions } from "fastify";
+import { FastifySchema, FastifyRequest as Request, RouteOptions } from "fastify";
 import urlCat from "urlcat";
+
+import { AxiosGamesPopular } from "@types";
 
 import { RAWG_BASE } from "@config";
 
-const handler = async () => {
+const schema: FastifySchema = {
+    querystring: {
+        type: "object",
+        properties: {
+            page: {
+                type: "number",
+                default: 1,
+            },
+        },
+    },
+};
+
+const handler = async (request: Request) => {
     const setMonth = (date: Date, by: number) =>
         new Date(date.setMonth(new Date(date).getMonth() + by));
 
@@ -13,21 +27,36 @@ const handler = async () => {
         "," +
         setMonth(new Date(), 0).toLocaleDateString("lt-LT");
 
-    const { data } = await axios.get(
+    const { page } = request.query as { [key: string]: string };
+
+    const { data } = await axios.get<AxiosGamesPopular>(
         urlCat(RAWG_BASE, "/games", {
             key: process.env.RAWG_KEY,
             platforms: "4",
             page_size: 30,
             ordering: "-added",
             dates,
+            page,
         })
     );
 
-    return { ...data, next: !!data.next, previous: !!data.previous };
+    // minify images
+    const results = data.results.map(({ background_image, ...rest }) => {
+        const split = background_image.split("/media/");
+        const minified = `${split[0]}/media/crop/600/400/${split[1]}`;
+
+        return {
+            background_image: minified,
+            ...rest,
+        };
+    });
+
+    return { ...data, results, next: !!data.next, previous: !!data.previous };
 };
 
 export default {
     method: "GET",
     url: "/games/popular",
     handler,
+    schema,
 } as RouteOptions;
