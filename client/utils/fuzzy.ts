dcdb7f13bce1b821c0454c2ada9ea167949aa500
Donatas Bucinskas <donatas@tronikel.com>
2022-02-07 23:02:33 +0200

    fuzzy search function

--- client/utils/fuzzy.ts
+++ client/utils/fuzzy.ts
@@ -1,11 +1,19 @@
+import type Fuse from "fuse.js";
+
 const flags = {
     skip: ["uncracked", "unlocked"],
 };
 
+// play around with settings to have a nice matching algo
+const settings: Fuse.IFuseOptions<string> = {
+    includeScore: true,
+    threshold: 0.05,
+    isCaseSensitive: true,
+};
+
 export default async function Fuzzy(list: string[], query: string) {
     const Fuse = (await import("fuse.js")).default;
-    // play around with settings to have a nice matching algo
-    const fuse = new Fuse(list, { includeScore: true, threshold: 0.05 });
+    const fuse = new Fuse(list, settings);
 
     // skip items with flags
     fuse.remove(item => flags.skip.some(flag => item.toLowerCase().includes(flag)));
