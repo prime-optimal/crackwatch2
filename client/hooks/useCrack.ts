de769f2f3fd0bcbd71318ac825497ad60933c409
Donatas Bucinskas <donatas@tronikel.com>
2022-02-08 13:05:07 +0200

    remove 1337x

--- client/hooks/useCrack.ts
+++ client/hooks/useCrack.ts
@@ -1,13 +1,12 @@
 import useSWR from "swr/immutable";
 
 import tryToCatch from "@utils/catch";
-import OneThreeThreeSeven from "@utils/searchers/1337x";
 import PcGamesTorrents from "@utils/searchers/pcgamestorrents";
 import Skidrow from "@utils/searchers/skidrow";
 
 const fetcher = async (name: string) => {
     const [result] = await tryToCatch(() =>
-        Promise.any([Skidrow(name), PcGamesTorrents(name), OneThreeThreeSeven(name)])
+        Promise.any([Skidrow(name), PcGamesTorrents(name)])
     );
     return !!result;
 };
