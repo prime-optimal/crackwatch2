b327edb50fcb0536b868645a319c2a4953542992
Donatas Bucinskas <donatas@tronikel.com>
2022-02-09 16:23:25 +0200

    make the footer a bit smaller

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -2,7 +2,14 @@ import { Typography } from "@mui/material";
 
 export function Footer() {
     return (
-        <Typography component="footer" color="text.secondary" align="center" mx={2} my={4}>
+        <Typography
+            variant="caption"
+            component="footer"
+            color="text.secondary"
+            align="center"
+            mx={2}
+            my={4}
+        >
             Credits: All of the information about the games and everything is provided by
             RAWG.io (https://rawg.io/apidocs)
             <br />
