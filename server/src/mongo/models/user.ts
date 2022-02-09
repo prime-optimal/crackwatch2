cdf710da42cfbbceea4d5d0de32b698e80b291d7
Donatas Bucinskas <donatas@tronikel.com>
2022-02-09 11:10:22 +0200

    add user in db

--- server/src/mongo/models/user.ts
+++ server/src/mongo/models/user.ts
@@ -9,7 +9,7 @@ export interface User {
 
 const userSchema = new Schema<User>({
     nickname: { type: String, required: true, minlength: 4, maxlength: 20 },
-    password: { type: String, required: true, minlength: 6, maxlength: 100 },
+    password: { type: String, required: true, minlength: 6, maxlength: 200 },
     email: { type: String, required: true, minlength: 4, maxlength: 100 },
     createdAt: { type: Date, required: true, default: () => new Date() },
 });
--- server/src/routes/auth/register.ts
+++ server/src/routes/auth/register.ts
@@ -1,6 +1,9 @@
 import { Static, Type } from "@sinclair/typebox";
-import { randomBytes } from "crypto";
+import { randomBytes, scrypt } from "crypto";
 import { FastifyRequest as Req, RouteOptions } from "fastify";
+import { promisify } from "util";
+
+import { userModel } from "@mongo";
 
 const body = Type.Object(
     {
@@ -12,8 +15,29 @@ const body = Type.Object(
 );
 type Body = Static<typeof body>;
 
+const randomBytesPromise = promisify(randomBytes);
+const scryptPromise = promisify(scrypt);
+
 const handler = async (req: Req<{ Body: Body }>) => {
-    return req.body;
+    const { email, nickname, password } = req.body;
+
+    if (await userModel.findOne({ $or: [{ email }, { nickname }] })) {
+        throw {
+            status: 400,
+            message: "User already exists",
+        };
+    }
+
+    const salt = await randomBytesPromise(16);
+    const hashed = (await scryptPromise(password, salt, 64)) as any as Buffer;
+
+    await userModel.create({
+        email,
+        nickname,
+        password: hashed.toString("base64"),
+    });
+
+    return "OK";
 };
 
 export default {
