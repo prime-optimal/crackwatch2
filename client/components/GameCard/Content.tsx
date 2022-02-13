8d8fc993e69e373f5b6dc34e058bcdd21b9ce22c
Donatas Bucinskas <donatas@tronikel.com>
2022-02-13 22:12:34 +0200

    lint

--- client/components/GameCard/Content.tsx
+++ client/components/GameCard/Content.tsx
@@ -1,40 +1,7 @@
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import HelpIcon from "@mui/icons-material/Help";
-import {
-    Box,
-    CardContent,
-    Chip,
-    CircularProgress,
-    CircularProgressProps,
-    Link as MuiLink,
-    Stack,
-    Typography,
-} from "@mui/material";
+import { CardContent, Chip, Link as MuiLink, Stack, Typography } from "@mui/material";
 import NextLink from "next/link";
-
-const CircularProgressWithLabel = (props: CircularProgressProps & { value: number }) => {
-    return (
-        <Box sx={{ position: "relative", display: "inline-flex" }}>
-            <CircularProgress variant="determinate" {...props} />
-            <Box
-                sx={{
-                    top: 0,
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    position: "absolute",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Typography variant="caption" component="div" color="text.secondary">
-                    {Math.round(props.value)}
-                </Typography>
-            </Box>
-        </Box>
-    );
-};
 
 interface ContentProps {
     slug: string;
