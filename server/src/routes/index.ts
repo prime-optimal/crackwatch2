7f9e99d0e46e8d55f3662b98835bd7046588682d
Donatas Bucinskas <donatas@tronikel.com>
2022-02-16 19:18:25 +0200

    rm unused route

--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Resource } from "fastify-autoroutes";
-
-export default (): Resource => ({
-    get: {
-        handler: (req, res) => {
-            res.send("Crackwatch 2 API");
-        },
-    },
-});
