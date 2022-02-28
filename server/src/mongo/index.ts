a1e35382a58d7a2982fc97d90200e43bd0630b97
Donatas Bucinskas <donatas@tronikel.com>
2022-02-28 23:40:51 +0200

    rm if

--- server/src/mongo/index.ts
+++ server/src/mongo/index.ts
@@ -4,12 +4,8 @@ const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/crac
 
 mongoose.plugin(require("@meanie/mongoose-to-json"));
 
-console.log(DATABASE_URL);
-
 export const getMongoClient = async () => {
-    if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(DATABASE_URL);
-    }
+    await mongoose.connect(DATABASE_URL);
     return mongoose.connection.getClient();
 };
 
