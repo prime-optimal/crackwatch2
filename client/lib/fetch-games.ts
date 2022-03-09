8920eb1b3efc906b050740db44a2a3383f8b926b
Donatas Bucinskas <donatas@tronikel.com>
2022-03-09 20:37:13 +0200

    dates var should be in function

--- client/lib/fetch-games.ts
+++ client/lib/fetch-games.ts
@@ -6,12 +6,12 @@ import { AxiosGames } from "@types";
 const pick = require("server/dist/utils/pick").default;
 const minifyImageSrc = require("server/dist/utils/minify").minifyImageSrc;
 
-const dates =
-    new Date(new Date().setMonth(new Date().getMonth() - 4)).toLocaleDateString("lt-LT") +
-    "," +
-    new Date().toLocaleDateString("lt-LT");
-
 export default async function FetchGames() {
+    const dates =
+        new Date(new Date().setMonth(new Date().getMonth() - 4)).toLocaleDateString("lt-LT") +
+        "," +
+        new Date().toLocaleDateString("lt-LT");
+
     const result = await axios.get<AxiosGames>(
         urlCat("https://api.rawg.io/api", "/games", {
             key: process.env.RAWG_KEY,
