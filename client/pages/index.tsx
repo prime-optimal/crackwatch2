976f3bbf4ae1f156c58b90159955bf2e688abc14
Donatas Bucinskas <donatas@tronikel.com>
2022-03-09 20:33:51 +0200

    add index page ssr

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,29 @@
+import { GetStaticProps } from "next";
+import { SWRConfig } from "swr";
+
+import { PageProps } from "@types";
+
 import Index from "@routes/idx";
 
-export default function Page() {
-    return <Index />;
+import FetchGames from "@lib/fetch-games";
+
+export const getStaticProps: GetStaticProps = async () => {
+    const result = await FetchGames();
+
+    return {
+        props: {
+            fallback: {
+                games: { ...result, next: true },
+            },
+        },
+        revalidate: 60 * 60 * 24,
+    };
+};
+
+export default function Page({ fallback }: PageProps) {
+    return (
+        <SWRConfig value={{ fallback }}>
+            <Index />
+        </SWRConfig>
+    );
 }
