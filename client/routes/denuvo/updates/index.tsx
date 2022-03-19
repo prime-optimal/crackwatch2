48d3dd6cd1f4d416619debca5c3db9fae6f369ec
Donatas Bucinskas <donatas@tronikel.com>
2022-03-20 00:45:25 +0200

    memo item for a bit of perf improvement

--- client/routes/denuvo/updates/index.tsx
+++ client/routes/denuvo/updates/index.tsx
@@ -11,24 +11,64 @@ import {
     Stack,
     Typography,
 } from "@mui/material";
+import { dequal } from "dequal";
+import { memo } from "react";
 import useSWRInfinite from "swr/infinite";
 import urlCat from "urlcat";
 
 import { AxiosDenuvoUpdates } from "@types";
 
+import { SWRImmutable } from "@config";
+
 import ResponsiveImage from "@components/ResponsiveImage";
 
 import useBreakpoint from "@hooks/useBreakpoint";
+
+const Item = memo(({ date, img, price, status, steam }: AxiosDenuvoUpdates["items"][0]) => {
+    const mobile = useBreakpoint("md");
+
+    return (
+        <Card>
+            <CardActionArea LinkComponent="a" href={steam} target="_blank">
+                <CardMedia sx={{ height: mobile ? 100 : 150 }}>
+                    <ResponsiveImage src={img} />
+                </CardMedia>
+
+                <CardContent sx={{ position: "relative" }}>
+                    <Typography gutterBottom>{status}</Typography>
+                    <Typography color="text.secondary" variant="body2">
+                        Updated on {date}
+                    </Typography>
+
+                    {price && (
+                        <Box
+                            bgcolor="secondary.main"
+                            position="absolute"
+                            right={0}
+                            bottom={0}
+                            p={0.3}
+                            sx={{
+                                borderTopLeftRadius: ({ shape }) => `${shape.borderRadius}px`,
+                                opacity: 0.85,
+                            }}
+                        >
+                            <Typography align="center">{price}</Typography>
+                        </Box>
+                    )}
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}, dequal);
 
 export default function DenuvoUpdates() {
     const { data, isValidating, setSize } = useSWRInfinite<AxiosDenuvoUpdates>(
         (index, prev) => {
             if (prev && !prev.next) return null;
             return urlCat("/denuvo/updates", { page: index + 1 });
-        }
+        },
+        SWRImmutable
     );
-
-    const mobile = useBreakpoint("md");
 
     const onClick = () => {
         setSize(x => x + 1);
@@ -43,45 +83,9 @@ export default function DenuvoUpdates() {
             <Stack spacing={3} alignItems="center">
                 <Grid container spacing={3}>
                     {data?.map(({ items }) =>
-                        items.map(({ date, img, price, status, steam }) => (
-                            <Grid item xs={6} md={4} lg={3} key={steam}>
-                                <Card>
-                                    <CardActionArea
-                                        LinkComponent="a"
-                                        href={steam}
-                                        target="_blank"
-                                    >
-                                        <CardMedia sx={{ height: mobile ? 100 : 150 }}>
-                                            <ResponsiveImage src={img} />
-                                        </CardMedia>
-
-                                        <CardContent sx={{ position: "relative" }}>
-                                            <Typography gutterBottom>{status}</Typography>
-                                            <Typography color="text.secondary" variant="body2">
-                                                Updated on {date}
-                                            </Typography>
-
-                                            {price && (
-                                                <Box
-                                                    bgcolor="secondary.main"
-                                                    position="absolute"
-                                                    right={0}
-                                                    bottom={0}
-                                                    p={0.3}
-                                                    sx={{
-                                                        borderTopLeftRadius: ({ shape }) =>
-                                                            `${shape.borderRadius}px`,
-                                                        opacity: 0.85,
-                                                    }}
-                                                >
-                                                    <Typography align="center">
-                                                        {price}
-                                                    </Typography>
-                                                </Box>
-                                            )}
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
+                        items.map(item => (
+                            <Grid item xs={6} md={4} lg={3} key={item.steam}>
+                                <Item {...item} />
                             </Grid>
                         ))
                     )}
@@ -89,7 +93,7 @@ export default function DenuvoUpdates() {
 
                 <Box>
                     <Button
-                        disabled={isValidating}
+                        disabled={isValidating || !data}
                         onClick={onClick}
                         variant="contained"
                         endIcon={<ExpandMoreIcon />}
